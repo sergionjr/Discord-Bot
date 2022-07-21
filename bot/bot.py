b3bb711579fc77d11c0b4e9e749ffc125feb7f8a
sergionjr <seno7509@colorado.edu>
2022-07-20 23:10:10 -0600

    Music Cog: play, pause, resume, skip, queue, clear, leave Help Cog: send_to_all, on_ready, help

--- bot/bot.py
+++ bot/bot.py
@@ -7,42 +7,44 @@ from discord.ext import commands
 from help_cog import help_cog
 from music_cog import music_cog
 
-f = open('../config.json')
+f = open('./config.json')
 data = json.load(f)
 TOKEN = data["TOKEN"]
 f.close()
 
 bot = commands.Bot(command_prefix=".")
 
+bot.remove_command("help")
+
 bot.add_cog(help_cog(bot))
 bot.add_cog(music_cog(bot))
 
-bot.run(os.getenv(TOKEN))
+bot.run(TOKEN)
 
 
 
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-
-@client.event
-async def on_message(message):
-    print(message)
-    user_name = str(message.author)
-    user_message = str(message.content)
-    channel = str(message.channel.name)
-    print("New Message:", user_message, "From:", user_name, "At:", message.created_at, "In Channel:", channel)
-
-    if message.author == client.user:
-        return
-    if user_message.lower() == '.hello':
-        await message.channel.send("Hello " + user_name)
-        return
-    elif user_message.lower() == '.random':
-        await message.channel.send("Here is your random number:" + str(random.randint(1, 100)))
-        return
-
-client.run(TOKEN)
+# @client.event
+# async def on_ready():
+#     print('We have logged in as {0.user}'.format(client))
+#
+#
+# @client.event
+# async def on_message(message):
+#     print(message)
+#     user_name = str(message.author)
+#     user_message = str(message.content)
+#     channel = str(message.channel.name)
+#     print("New Message:", user_message, "From:", user_name, "At:", message.created_at, "In Channel:", channel)
+#
+#     if message.author == client.user:
+#         return
+#     if user_message.lower() == '.hello':
+#         await message.channel.send("Hello " + user_name)
+#         return
+#     elif user_message.lower() == '.random':
+#         await message.channel.send("Here is your random number:" + str(random.randint(1, 100)))
+#         return
+#
+# client.run(TOKEN)
--- bot/music_cog.py
+++ bot/music_cog.py
@@ -23,7 +23,7 @@ class music_cog(commands.Cog):
                 info = ydl.extract_info("ytsearch:%s" % item, download=False)['entries'][0]
             except Exception:
                 return False
-        return {'source': info['formats'[0]['url']], 'title': info['title']}
+        return {'source': info['formats'][0]['url'], 'title': info['title']}
 
     def play_next(self):
         if len(self.music_queue) > 0:
@@ -37,7 +37,7 @@ class music_cog(commands.Cog):
         else:
             self.is_playing = False
 
-    def play_music(self, ctx):
+    async def play_music(self, ctx):
         if len(self.music_queue) > 0:
             self.is_playing = True
             m_url = self.music_queue[0][0]['source']
@@ -46,7 +46,7 @@ class music_cog(commands.Cog):
                 self.vc = await self.music_queue[0][1].connect()
 
                 if self.vc == None:
-                    await ctx.send("You must be in a channel to use this command")
+                    await ctx.send("Failed to join a voice channel")
                     return
             else:
                 await self.vc.move_to(self.music_queue[0][1])
@@ -56,3 +56,73 @@ class music_cog(commands.Cog):
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
         else:
             self.is_playing = False
+
+    @commands.command(name="play", aliases=["p", "playing"], help=".play (song) will search (song) on youtube and play the given URL")
+    async def play(self, ctx, *args):
+        query = " ".join(args)
+
+        voice_channel = ctx.author.voice.channel
+        if voice_channel is None:
+            await ctx.send("You must be connected to a voice channel!")
+        elif self.is_paused:
+            self.vc.resume()
+        else:
+            song = self.search_yt(query)
+            if type(song) == type(True):
+                await ctx.send("Could not download the song. Incorrect format, try a different keyword")
+            else:
+                await ctx.send("Song has been added to the queue")
+                self.music_queue.append([song, voice_channel])
+
+                if self.is_playing == False:
+                    await self.play_music(ctx)
+
+    @commands.command(name="pause", help=".pause (song) will pause the current song being played")
+    async def pause(self, ctx, *args):
+        if self.is_playing:
+            self.is_playing = False
+            self.is_paused = True
+            self.vc.pause()
+        elif self.is_paused:
+            self.is_playing = True
+            self.is_paused = False
+            self.vc.resume()
+
+    @commands.command(name="resume", aliases=["r"], help="Resumes playing the current song")
+    async def resume(self, ctx, *args):
+        if self.is_paused:
+            self.is_playing = True
+            self.is_paused = False
+            self.vc.resume()
+
+    @commands.command(name="skip", aliases=["s"], help="Skips the currently played song")
+    async def skip(self, ctx, *args):
+        if (self.vc != None) and (self.vc):
+            self.vc.stop()
+            await self.play_music(ctx)
+
+    @commands.command(name="queue", aliases=["q"], help="Displays all the songs currently in queue")
+    async def queue(self, ctx):
+        retval = ""
+
+        for i in range(0, len(self.music_queue)):
+            if (i>4): break
+            retval += self.music_queue[i][0]['title'] + '\n'
+
+        if retval != "":
+            await ctx.send(retval)
+        else:
+            await ctx.send("No music currently in the queue.")
+
+    @commands.command(name="clear", help="Stops the current song and clears the queue")
+    async def clear(self, ctx, *args):
+        if (self.vc != None) and (self.is_playing):
+            self.vc.stop()
+        self.music_queue = []
+        await ctx.send("Music queue has been cleared")
+
+    @commands.command(name="quit", help="Kicks the bot from the voice channel")
+    async def leave(self, ctx):
+        self.is_playing = False
+        self.is_paused = False
+        await self.vc.disconnect()
