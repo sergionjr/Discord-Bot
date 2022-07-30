084e304ec906b3b5f812fa6f5ece723c4ebfc7c5
sergionjr <seno7509@colorado.edu>
2022-07-30 09:43:59 -0600

    Reminder function skeletons: reminder_add(), reminder_modify(), reminder_delete, reminder_delete_all
    Signed-off-by: sergionjr <seno7509@colorado.edu>

--- bot/reminder_cog.py
+++ bot/reminder_cog.py
@@ -21,17 +21,17 @@ default_app = firebase_admin.initialize_app(cred, {
 
 ref = db.reference('/Database reference')
 
+
 dict_entry = """{
 "reminderID": "reminderEntry"
 }"""
 
 secondary_dict = {
     'rockstar' : 'axel'
+    'userID'
 }
 
 dict_entry = json.loads(dict_entry) #json loads must load a dictionary string. The triple quotes prep it.
-print(dict_entry)
-print(type(dict_entry))
 ref.push(secondary_dict)
 
 print(ref.get())
@@ -55,16 +55,54 @@ class reminder_cog(commands.Cog):
         self.bot = bot
         self.text_channel_text = []
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print()
+    async def reminder_add(self, ctx):
+        await ctx.send("reminder_add")
+        return
+
+    async def reminder_modify(self, ctx):
+        await ctx.send("reminder_modify")
+        return
+
+    async def reminder_delete(self, ctx):
+        await ctx.send("reminder_delete")
+        return
+
+    async def reminder_delete_all(self, ctx):
+        await ctx.send("reminder_delete_all")
+        return
+
+
+
+    # @commands.Cog.listener()
+    # async def on_ready(self):
+    #     print()
+
+
 
     @commands.command(name="remind", aliases=["alert"], help="test test")
-    async def reminder(self, ctx, arg1):
-        print("1 arg reminder function", arg1)
-        await ctx.send(arg1)
+    async def reminder(self, ctx, *args):
+        reminder_operations = {
+            'add': self.reminder_add,
+            'modify': self.reminder_modify,
+            'delete': self.reminder_delete,
+            'deleteall': self.reminder_delete_all
+        }
+        await reminder_operations[args[0]](ctx)
 
-    @commands.command(name="remind", aliases=["alert"], help="test test")
-    async def reminder(self, ctx, arg1, arg2, arg3):
-        print("3 arg reminder function", arg1, arg2, arg3)
+        # await ctx.send("argument 1: " + args[0])
+        # await ctx.send(args)
+
+    @commands.command(name="reminders", help="push a reminder to firebase")
+    async def reminder_push(self, ctx, *args):
+        await ctx.send("Args provided:" + " ".join(args))
+        print(args)
+
+    @commands.command(name="retrieve", help="filler")
+    async def retrieve(self, ctx):
+        await ctx.send(str(ref.get()))
+
+
+    # @commands.Cog.listener()
+    # async def on_message(self):
+    #     return
 
