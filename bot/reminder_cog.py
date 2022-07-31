bd43915fe42430fed6e8129092e86fe3716f869c
sergionjr <seno7509@colorado.edu>
2022-07-31 17:34:30 -0600

    populate function for dummy reminder entries
    Signed-off-by: sergionjr <seno7509@colorado.edu>

--- bot/reminder_cog.py
+++ bot/reminder_cog.py
@@ -2,13 +2,13 @@ import discord
 import firebase_admin
 import json
 import os
+import datetime
 
 # External
 from discord.ext import commands
 from firebase_admin import credentials
 from firebase_admin import db
-
-# Internal
+from datetime import date
 
 
 serviceAccountKey = json.loads(os.environ['serviceAccountKey'])
@@ -21,38 +21,30 @@ default_app = firebase_admin.initialize_app(cred, {
 
 ref = db.reference('/Reminders (Test)')
 
+print(date.today() + datetime.timedelta(days=1))
 
+print(type(date.today()))
 
-
-
-
-#print("ref:", ref, type(ref))
-
-
-dict_entry = """{
-"reminderID": "reminderEntry"
-}"""
-
-secondary_dict = {
-    'rockstar' : 'axel',
-    'userID' : '155232'
-}
 
 #dict_entry = json.loads(dict_entry) #json loads must load a dictionary string. The triple quotes prep it.
 #ref.push(secondary_dict)
 
-
-
-
 class reminder:
-    def __init__(self, server_id, user_ids, reminder, reminder_date, recurring, recurring_frequency):
-
-        self.server_id = server_id
-        self.user_id = user_ids
-        self.reminder = reminder
-        self.reminder_date = reminder_date
+    def __init__(self, description, date, created_on, recurring, recurring_frequency):
+        self.description = description
+        self.date = date
+        self.created_on = created_on
         self.recurring = recurring #true or false
         self.recurring_frequency = recurring_frequency
+
+    def to_dictionary(self):
+        return {
+            "description": self.description,
+            "date" : str(self.date),
+            "created_on": str(self.created_on),
+            "recurring": self.recurring,
+            "recurring_frequency" : self.recurring_frequency
+        }
 
 class reminder_exo:
     def __init__(self, reminder_id, server_id, user_id):
@@ -96,14 +88,6 @@ class reminder_cog(commands.Cog):
             await ctx.send(f"{ctx.author.mention}: Failed to clear your reminders")
         return
 
-
-
-    # @commands.Cog.listener()
-    # async def on_ready(self):
-    #     print()
-
-
-
     @commands.command(name="reminder", aliases=["alert"], help="test test")
     async def reminder(self, ctx, *args):
         reminder_operations = {
@@ -116,8 +100,6 @@ class reminder_cog(commands.Cog):
             await reminder_operations[args[0]](ctx)
         except:
             await ctx.send(f"{ctx.message.author.mention}: Command not recognized.")
-       # await ctx.send("argument 1: " + args[0])
-       # await ctx.send(args)
 
     @commands.command(name="reminders", help="push a reminder to firebase")
     async def reminder_push(self, ctx, *args):
@@ -126,10 +108,9 @@ class reminder_cog(commands.Cog):
         reminder_id, server_id, user_id = args #sets them in order of how they are in the args[] structure.
         reminder = reminder_exo(reminder_id, server_id, user_id) #instantiates class
 
-        #Hierarchy: "Reminders (Test)" / "UserID" / "Server ID" /"Reminder Dictionaries"
+        #Hierarchy: "Reminders (Test)" / "ServerName:ServerID" / "UserName:UserID" /"Reminder Dictionaries"
         #ref.child(<userid>/<reminderid>)
         ref.child(f"{ctx.guild.name}:{ctx.guild.id}/{ctx.author.name}:{ctx.author.id}").push(reminder.to_dictionary())
-        #print(reminder)
 
     @commands.command(name="myreminders", aliases=["myr"], help="filler")
     async def retrieve(self, ctx):
@@ -140,10 +121,37 @@ class reminder_cog(commands.Cog):
             return
 
         message = f"Here are your reminders {ctx.message.author.mention}:"
+        n = 1
         for key in user_reminders.keys():
-            message += f"\n {user_reminders[key]}"
+            message += f"\n {n}. Reminder ID: {key}, [date] {user_reminders[key]}"
+            n += 1
         await ctx.send(message)
 
+    @commands.command(name="populate", aliases=["pop"], help="filler")
+    async def populate(self, ctx):
+        reminder_1 = reminder(description = "New league prime capsule",
+                              date = date(2022, 8, 30),
+                              created_on = datetime.date.today(),
+                              recurring = True,
+                              recurring_frequency = 'monthly')
+
+        reminder_2 = reminder(description = "Groceries",
+                              date = datetime.date(2022, 8, 2),
+                              created_on = datetime.date.today(),
+                              recurring = False,
+                              recurring_frequency = 'none')
+
+        reminder_3 = reminder(description = "Take the chicken out of the oven",
+                              date = date(2022, 9, 5),
+                              created_on = datetime.date.today(),
+                              recurring = True,
+                              recurring_frequency = 'weekly')
+        reminder_arr = [reminder_1, reminder_2, reminder_3]
+
+        for rem in reminder_arr:
+            ref.child(f"{ctx.guild.name}:{ctx.guild.id}/{ctx.author.name}:{ctx.author.id}").push(rem.to_dictionary())
+
+        return
 
 
     #@commands.Cog.description
