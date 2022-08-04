f95d70f61076bd0bc451f4b8b34c1e83194a4d54
sergionjr <seno7509@colorado.edu>
2022-08-03 23:34:07 -0600

    Dev token for dev branch
    Signed-off-by: sergionjr <seno7509@colorado.edu>

--- bot/bot.py
+++ bot/bot.py
@@ -15,7 +15,7 @@ from music_cog import music_cog
 from reminder_cog import reminder_cog
 
 
-TOKEN = os.environ['DEVELOPMENT_TOKEN']
+TOKEN = os.environ['TOKEN']
 
 bot = commands.Bot(command_prefix=".")
 
